38766d15b92176490f0f3cac0167f9633671d750
LeeWongeun <94237345+LeeWongeun@users.noreply.github.com>
2024-03-26 19:16:19 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 312 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,20 +1,72 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	int arr[1000000], n;
-	
-	cin >> n;
+static vector<int> vec;
+static vector<int> temp;
 
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+void mergeSort(int start, int end)
+{
+	if (start >= end)
+	{
+		return;
 	}
 
-	sort(arr, arr + n);
+	int m = (start + end) / 2;
+	mergeSort(start, m);
+	mergeSort(m + 1, end);
 
-	for (int i = 0; i < n; i++) {
-		cout << arr[i] << "\n";
+	for (int i = start; i <= end; i++)
+	{
+		temp[i] = vec[i];
+	}
+
+	int index1 = start, index2 = m + 1;
+	int i = start;
+	while (index1 < m + 1 && index2 <= end)
+	{
+		if (temp[index1] < temp[index2])
+		{
+			vec[i] = temp[index1++];
+		}
+		else
+		{
+			vec[i] = temp[index2++];
+		}
+		i++;
+	}
+	while (index1 < m + 1)
+	{
+		vec[i] = temp[index1++];
+		i++;
+	}
+	while (index2 <= end)
+	{
+		vec[i] = temp[index2++];
+		i++;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	vec = vector<int>(n);
+	temp = vector<int>(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i];
+	}
+
+	mergeSort(0, n - 1);
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << vec[i] << '\n';
 	}
 }
