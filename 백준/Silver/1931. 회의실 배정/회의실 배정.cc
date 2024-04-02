a6db0d9dd961573301f3c00897cea8e33ea1143b
LeeWongeun <94237345+LeeWongeun@users.noreply.github.com>
2024-04-02 15:53:30 +0900

    [Silver I] Title: 회의실 배정, Time: 32 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,41 +1,46 @@
-#include <iostream>
+#include<iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-struct lecture {
-	int start = 0;
-	int end = 0;
-};
+int n;
+vector<pair<int,int>> vec;
 
-bool compare(lecture a, lecture b) {
-	if (a.end == b.end) {
-		return a.start < b.start;
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
 	}
-	return a.end < b.end;
+	return a.second < b.second;
 }
-int main() {
-	vector<lecture> arr;
-	int n, cnt = 0 , last = 0;
-	lecture lec;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
 	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		cin >> lec.start >> lec.end;
-		arr.push_back(lec);
+	vec.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i].first;
+		cin >> vec[i].second;
 	}
+	sort(vec.begin(), vec.end(), cmp);
 
-	sort(arr.begin(), arr.end(), compare);
-
-	last = arr[0].end;
-	cnt++;
-	for (int i = 1; i < n; i++) {
-		if (arr[i].start >= last) {
+	int cnt = 0;
+	int endTime = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (vec[i].first >= endTime)
+		{
 			cnt++;
-			last = arr[i].end;
+			endTime = vec[i].second;
 		}
 	}
+
 	cout << cnt;
 }
