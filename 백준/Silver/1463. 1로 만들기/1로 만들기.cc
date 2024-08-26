304d8e163e75fb0dbfc3b4a7ee4b76d431c53d9a
LeeWongeun <94237345+LeeWongeun@users.noreply.github.com>
2024-08-26 14:53:39 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,23 +1,43 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int a[1000001] = {0,0,1,1};
+int main()
+{
+	int n;
+	cin >> n;
 
-int func(int N) {
-	int x;
-	if (N == 1 || N == 0) return 0;
-	else if (a[N] != 0) return a[N];
-	else if (N > 3) {
-		x = 1 + func(N - 1);
-		if (N % 3 == 0) x = min(x, a[N / 3] + 1);
-		if (N % 2 == 0) x = min(x, a[N / 2] + 1);
-		a[N] = x;
-		return x;
+	vector<int> vec(n + 1, 1e9);
+	vec[0] = 0;
+	vec[1] = 0;
+	
+	for (int i = 1; i <= n; ++i)
+	{
+		if (i + 1 <= n)
+		{
+			if (vec[i] + 1 < vec[i + 1])
+			{
+				vec[i + 1] = vec[i] + 1;
+			}
+		}
+
+		if (i * 2 <= n)
+		{
+			if (vec[i] + 1 < vec[i * 2])
+			{
+				vec[i * 2] = vec[i] + 1;
+			}
+		}
+
+		if (i * 3 <= n)
+		{
+			if (vec[i] + 1 < vec[i * 3])
+			{
+				vec[i * 3] = vec[i] + 1;
+			}
+		}
 	}
-}
-int main() {
-	int N = 0;
-	cin >> N;
-	cout << func(N);
+
+	cout << vec[n];
 }
