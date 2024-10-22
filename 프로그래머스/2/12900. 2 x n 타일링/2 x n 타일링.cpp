0e3ddf715f4ea00fd3ba2da0d6830024946f53ba
LeeWongeun <94237345+LeeWongeun@users.noreply.github.com>
2024-10-22 14:23:14 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.27 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v(n+1);
+    v[1] = 1;
+    v[2] = 2;
+    v[3] = 3;
+
+    for(int i=4; i<=n; i++)
+    {
+        v[i] = (v[i-1] + v[i-2])%1000000007;
+    }
+
+    answer = v[n];
+    return answer;
+}
